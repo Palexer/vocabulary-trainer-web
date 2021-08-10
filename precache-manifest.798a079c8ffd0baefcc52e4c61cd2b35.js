b194929df8fe581648f88745b9ab63f32a44b149
Palexer <palexer11@gmail.com>
2021-08-10 16:34:04 +0200

    deploy

---
+++ precache-manifest.798a079c8ffd0baefcc52e4c61cd2b35.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909dc20d90bc3755252f",
+    "url": "/vocabulary-trainer-web/css/app.3cdaf8fd.css"
+  },
+  {
+    "revision": "1bb69248a99dc4771206",
+    "url": "/vocabulary-trainer-web/css/chunk-015cbdfb.d727ac89.css"
+  },
+  {
+    "revision": "cb2b4d00f9268b54c328",
+    "url": "/vocabulary-trainer-web/css/chunk-11088dfd.33b85c19.css"
+  },
+  {
+    "revision": "cd82905a4c6703ab978a",
+    "url": "/vocabulary-trainer-web/css/chunk-995382b8.039f4d24.css"
+  },
+  {
+    "revision": "db13478eda418955ddf6",
+    "url": "/vocabulary-trainer-web/css/chunk-b9df962e.150c71b9.css"
+  },
+  {
+    "revision": "e586ca252dfa50ea1d060bae4f49df28",
+    "url": "/vocabulary-trainer-web/index.html"
+  },
+  {
+    "revision": "909dc20d90bc3755252f",
+    "url": "/vocabulary-trainer-web/js/app.816af8e8.js"
+  },
+  {
+    "revision": "1bb69248a99dc4771206",
+    "url": "/vocabulary-trainer-web/js/chunk-015cbdfb.ef82cffb.js"
+  },
+  {
+    "revision": "cb2b4d00f9268b54c328",
+    "url": "/vocabulary-trainer-web/js/chunk-11088dfd.a1e501ec.js"
+  },
+  {
+    "revision": "cd82905a4c6703ab978a",
+    "url": "/vocabulary-trainer-web/js/chunk-995382b8.2449fb86.js"
+  },
+  {
+    "revision": "db13478eda418955ddf6",
+    "url": "/vocabulary-trainer-web/js/chunk-b9df962e.20b09474.js"
+  },
+  {
+    "revision": "da7b7e66437267b5ce9c",
+    "url": "/vocabulary-trainer-web/js/chunk-vendors.ce2a3148.js"
+  },
+  {
+    "revision": "c8631b649aee91354c79e77eaa5752aa",
+    "url": "/vocabulary-trainer-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vocabulary-trainer-web/robots.txt"
+  }
+]);
